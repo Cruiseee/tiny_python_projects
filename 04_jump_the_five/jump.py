cc7e61a518d4d4007ba359e15f5a7b34fe265236
Cruiseee <dangerousdeepak25@gmail.com>
2022-10-30 16:26:48 +0530

    format changes

--- 04_jump_the_five/jump.py
+++ 04_jump_the_five/jump.py
@@ -13,19 +13,20 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Jump the five algorithm',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Jump the five algorithm",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('ctext',
-                        metavar='str',
-                        help='cryptic text')
+    parser.add_argument("ctext", metavar="str", help="cryptic text")
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
+    parser.add_argument(
+        "-a",
+        "--arg",
+        help="A named string argument",
+        metavar="str",
+        type=str,
+        default="",
+    )
 
     return parser.parse_args()
 
@@ -37,9 +38,20 @@ def main():
     args = get_args()
     str_arg = args.ctext
 
-    num_jump = {'1':'9', '2':'8', '3':'7', '4':'6', '5':'0', '6':'4', '7':'3', '8':'2', '9':'1', '0':'5'}
+    num_jump = {
+        "1": "9",
+        "2": "8",
+        "3": "7",
+        "4": "6",
+        "5": "0",
+        "6": "4",
+        "7": "3",
+        "8": "2",
+        "9": "1",
+        "0": "5",
+    }
 
-    decode_text = ''
+    decode_text = ""
 
     for char in str_arg:
         if char.isnumeric():
@@ -48,10 +60,9 @@ def main():
         else:
             decode_text += char
 
-    
-    print(f'{decode_text}')
+    print(f"{decode_text}")
 
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
